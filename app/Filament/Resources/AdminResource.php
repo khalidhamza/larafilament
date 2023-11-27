cc6fc22f974b27c14a94b1ef88b35b78d356ad31
Khalid Hamza <khalid_hamza2015@yahoo.com>
2023-11-28 01:14:05 +0300

    add admins resource

---
+++ app/Filament/Resources/AdminResource.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\AdminResource\Pages;
+use App\Filament\Resources\AdminResource\Pages\EditAdmin;
+use App\Filament\Resources\AdminResource\RelationManagers;
+use App\Models\Admin;
+use Filament\Forms;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Forms\Get;
+use Filament\Pages\Page;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Actions\DeleteAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class AdminResource extends Resource
+{
+    protected static ?string $model = Admin::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-users';
+
+    protected static ?string $navigationGroup = 'Admins Management';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Section::make()->schema([
+                    TextInput::make('name')->required(),
+                    TextInput::make('email')
+                        ->required()
+                        ->email(),
+                    Select::make('role')
+                        ->required()
+                        ->searchable()
+                        ->preload()
+                        ->relationship('roles', 'name'),
+                    TextInput::make('password')
+                        ->required()
+                        ->password()
+                        ->hidden(fn (Get $get) => !empty($get('id')))
+                ])->columns(2),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')->searchable()->sortable(),
+                TextColumn::make('email')->searchable()->sortable(),
+                TextColumn::make('roles.name')->searchable()->sortable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListAdmins::route('/'),
+            'create' => Pages\CreateAdmin::route('/create'),
+            'edit' => Pages\EditAdmin::route('/{record}/edit'),
+        ];
+    }
+}
